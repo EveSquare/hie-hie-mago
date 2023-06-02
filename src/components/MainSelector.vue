115779f8b4c813e379f7edcd9e8ad3488c5171cf
EveSquare <50450074+EveSquare@users.noreply.github.com>
2023-06-02 14:40:10 +0900

    😁

--- src/components/MainSelector.vue
+++ src/components/MainSelector.vue
@@ -1,53 +1,8 @@
 <template>
     <select :value="selector" @change="$emit('change', index, $event.target.value)">
         <option value="">🕵🏻‍♂️</option>
-        <optgroup label="家族">
-            <option>兄弟姉妹</option>
-            <option>母</option>
-            <option>父</option>
-            <option>祖父母</option>
-            <option>子供</option>
-            <option>祖先</option>
-            <option>孫</option>
-        </optgroup>
-        <optgroup label="親戚">
-            <option>親戚</option>
-            <option>叔父・叔母</option>
-            <option>いとこ</option>
-            <option>甥・姪</option>
-        </optgroup>
-        <optgroup label="友人・仲間">
-            <option>友人</option>
-            <option>知人</option>
-            <option>仲間</option>
-            <option>クラスメート</option>
-            <option>チームメイト</option>
-            <option>同僚</option>
-            <option>先輩</option>
-            <option>後輩</option>
-        </optgroup>
-        <optgroup label="恋愛関係">
-            <option>配偶者</option>
-            <option>パートナー</option>
-            <option>片思いの相手</option>
-            <option>元夫・元妻</option>
-            <option>婚約者</option>
-        </optgroup>
-        <optgroup label="教育関係">
-            <option>先生</option>
-            <option>生徒</option>
-            <option>メンター</option>
-            <option>メンティー</option>
-            <option>担任教師</option>
-            <option>見習い</option>
-        </optgroup>
-        <optgroup label="その他の関係">
-            <option>上司</option>
-            <option>部下</option>
-            <option>保護者</option>
-            <option>知的パートナー</option>
-            <option>面会人</option>
-            <option>スポンサー</option>
+        <optgroup v-for="optgroup in optgroups" :label="optgroup.label">
+            <option v-for="data in optgroup.data" :value="data">{{ data }}</option>
         </optgroup>
     </select>
     <button @click="$emit('removeSelector', index)">☠︎</button>
@@ -64,6 +19,96 @@ export default {
             type: String,
             required: true,
         },
+    },
+    data () {
+        return {
+            optgroups: [
+                {
+                    label: "家族",
+                    data: [
+                        "兄",
+                        "弟",
+                        "姉",
+                        "妹",
+                        "母",
+                        "父",
+                        "祖父母",
+                        "子供",
+                        "祖先",
+                        "孫",
+                    ],
+                },
+                {
+                    label: "親戚",
+                    data: [
+                        "親戚",
+                        "叔父",
+                        "叔母",
+                        "いとこ",
+                        "甥",
+                        "姪",
+                    ],
+                },
+                {
+                    label: "友人・仲間",
+                    data: [
+                        "友人",
+                        "知人",
+                        "仲間",
+                        "クラスメート",
+                        "チームメイト",
+                        "同僚",
+                        "先輩",
+                        "後輩",
+                    ],
+                },
+                {
+                    label: "恋愛関係",
+                    data: [
+                        "配偶者",
+                        "パートナー",
+                        "片思いの相手",
+                        "婚約者",
+                        "元夫",
+                        "元妻",
+                    ], 
+                },
+                {
+                    label: "教育関係",
+                    data: [
+                        "先生",
+                        "生徒",
+                        "メンター",
+                        "担任教師",
+                        "見習い",
+                    ],
+                },
+                {
+                    label: "その他の関係",
+                    data: [
+                        "上司",
+                        "部下",
+                        "従業員",
+                        "保護者",
+                        "知的パートナー",
+                        "面会人",
+                        "スポンサー",
+                        "共同研究者",
+                        "義兄弟",
+                        "義姉妹",
+                        "隠し子",
+                        "心の支え",
+                        "戦友",
+                        "お世話になった人",
+                        "見守り人",
+                        "仲直り相手",
+                        "スポーツライバル",
+                        "目標とする人",
+                        "ライバル",
+                    ],
+                },
+            ]
+        }
     },
 }
 
